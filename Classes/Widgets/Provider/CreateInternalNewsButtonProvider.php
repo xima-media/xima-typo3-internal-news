9c794bda69b5076e0c32b419e217ab308da883cb
Konrad Michalik <km@move-elevator.de>
2025-07-31 13:05:44 +0200

    build: adjust php-cs-fixer rules

--- Classes/Widgets/Provider/CreateInternalNewsButtonProvider.php
+++ Classes/Widgets/Provider/CreateInternalNewsButtonProvider.php
@@ -2,6 +2,25 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TYPO3 CMS extension "xima_typo3_internal_news".
+ *
+ * Copyright (C) 2025 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace Xima\XimaTypo3InternalNews\Widgets\Provider;
 
 use TYPO3\CMS\Backend\Routing\UriBuilder;
@@ -11,9 +30,7 @@ use TYPO3\CMS\Dashboard\Widgets\ElementAttributesInterface;
 
 class CreateInternalNewsButtonProvider implements ButtonProviderInterface, ElementAttributesInterface
 {
-    public function __construct(private readonly string $title, private readonly string $target = '')
-    {
-    }
+    public function __construct(private readonly string $title, private readonly string $target = '') {}
 
     public function getTitle(): string
     {
